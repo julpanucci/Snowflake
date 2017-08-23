3e33def5d28afe6811cfe361cdd480dc6e26acbc
Julian Panucci <julpanucci@gmail.com>
2017-08-23 15:29:16 -0400

    Updated version number

--- Example/Snowflake/ViewController.swift
+++ Example/Snowflake/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)
+        
         let flake = #imageLiteral(resourceName: "flake")
         
         let snowflake = Snowflake(view: view, particles: [flake: .white])
--- Snowflake.podspec
+++ Snowflake.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'Snowflake'
-  s.version          = '0.1.0'
+  s.version          = '0.2.0'
   s.summary          = 'Let it snow, let it snow, let it snow ❄️'
 
 # This description is used to generate tags and improve search results.
